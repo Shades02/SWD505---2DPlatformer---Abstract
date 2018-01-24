721aced2572f18f54f6aa697f1102145680a4177
Alex Pearson <3peara60@solent.ac.uk>
2018-01-24 17:28:34 +0000

    Initial Prototyping
    Very basic stuff atm

---
+++ Abstract Game/Assets/Player_Script.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Script : MonoBehaviour
+{
+    public int moveSpeed;
+    Rigidbody2D myRigid;
+
+	void Start ()
+    {
+        myRigid = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        myRigid.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, myRigid.velocity.y);
+    }
+}
