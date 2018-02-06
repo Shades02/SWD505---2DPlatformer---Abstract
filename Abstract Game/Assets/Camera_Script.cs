5872399a19280dd85179cb8de6e8c9786925669f
Alex Pearson <3peara60@solent.ac.uk>
2018-02-06 15:18:10 +0000

    Camera
    wip

--- Abstract Game/Assets/Camera_Script.cs
+++ Abstract Game/Assets/Camera_Script.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera_Script : MonoBehaviour {
+public class Camera_Script : MonoBehaviour
+{
+    private GameObject player;
 
-	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
 	}
 }
