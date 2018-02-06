bd567e318959d3b7248f0444db7169c125df7c96
Alex Pearson <3peara60@solent.ac.uk>
2018-02-06 16:24:26 +0000

    CameraLock
    Current start in bottom left(i.e camer wont go down or left, only up and right), can be adjusted based on level design

--- Abstract Game/Assets/Camera_Script.cs
+++ Abstract Game/Assets/Camera_Script.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class Camera_Script : MonoBehaviour
 {
     private GameObject player;
+    private Vector2 origin;
+    public float maxX;
 
-	void Start ()
+    void Start ()
     {
+        origin = transform.position;
         player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	void Update ()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
 	}
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, origin.x, maxX),              //left point = origin   right point = maxX
+            Mathf.Clamp(transform.position.y, origin.y, 99),                //bottom point = origin     top point = 99
+            -10);                                                           //-10
+    }
 }
