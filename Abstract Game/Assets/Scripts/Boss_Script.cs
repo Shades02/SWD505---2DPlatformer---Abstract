79dd00c1a4ba6545bc61fdaae1d80959929bbf77
David Mincer <david.r.mincer@live.com>
2018-03-03 14:52:34 +0000

    Prefabs and colliders

---
+++ Abstract Game/Assets/Scripts/Boss_Script.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Script : MonoBehaviour {
+
+    public float moveSpeed,
+        minX,
+        maxX;
+    public GameObject[] shapes;
+
+
+}
