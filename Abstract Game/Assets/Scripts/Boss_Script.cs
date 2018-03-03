9c1cdafce1dd668a204c617be22a5b15796d2487
David Mincer <david.r.mincer@live.com>
2018-03-03 15:50:23 +0000

    Quick commit

--- Abstract Game/Assets/Scripts/Boss_Script.cs
+++ Abstract Game/Assets/Scripts/Boss_Script.cs
@@ -9,5 +9,29 @@ public class Boss_Script : MonoBehaviour {
         maxX;
     public GameObject[] shapes;
 
+    private Vector2 moveDirection;
+    private bool patrolling = false;
 
+    private void Start()
+    {
+        moveDirection = new Vector2(-1, 0);
+    }
+
+    private void patrol()       //Boss moves left and right within boundaries
+    {
+        if (!patrolling) GetComponent<Rigidbody2D>().velocity = moveDirection * moveSpeed;
+        else if (transform.position.x >= maxX || transform.position.x <= minX)
+        {
+            moveDirection = -moveDirection;
+            patrolling = false;
+        }
+    }
+
+    private void setShapeVelocitytoPoint(Vector2 point)
+    {
+        for (int i = 0; i < shapes.Length; ++i)
+        {
+
+        }
+    }
 }
