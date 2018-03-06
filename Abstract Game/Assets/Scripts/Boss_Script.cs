26fef198689b076451c242ae55c7c6fef1d61e83
David Mincer <david.r.mincer@live.com>
2018-03-06 00:19:44 +0000

    Start Melee

--- Abstract Game/Assets/Scripts/Boss_Script.cs
+++ Abstract Game/Assets/Scripts/Boss_Script.cs
@@ -12,12 +12,17 @@ public class Boss_Script : Enemy_Script
         p1OrbitSpeed,
         p2OrbitSpeed,
         p1ShootCD,
-        p2ShootCD;
+        p2ShootCD,
+        maxMeleeCD;
     public GameObject[] shapes;
 
     private Vector2 moveDirection;
-    private bool patrolling = false;
+    private Vector3 targetPosition;
+    private bool patrolling = false,
+        shooting = false,
+        meleeAttacking = false;
     private int p2Health;
+    private float meleeCD;
 
     private void Start()
     {
@@ -35,7 +40,8 @@ public class Boss_Script : Enemy_Script
     private void FixedUpdate()
     {
         orbitShapes();
-        for (int i = 0; i < shapes.Length; ++i) shapes[i].GetComponent<Shape_Script>().shoot();
+        if (shooting)
+            for (int i = 0; i < shapes.Length; ++i) shapes[i].GetComponent<Shape_Script>().shoot();
 
         if (health > p2Health)
         {
@@ -77,6 +83,40 @@ public class Boss_Script : Enemy_Script
         for (int i = 0; i < shapes.Length; ++i) shapes[i].GetComponent<Shape_Script>().moveShape();
     }
 
+    private void meleeAttack()      //Charges at player
+    {
+        if (meleeCD <= 0)
+        {
+            shooting = false;
+            patrolling = false;
+
+            if (!meleeAttacking)
+            {
+                setShapeDirectiontoPoint(transform.position);
+
+                if (shapesInCentre())
+                {
+                    targetPosition = player.transform.position;
+                    setShapeDirectiontoPoint(targetPosition);
+                    meleeAttacking = true;
+                }
+            }
+            else
+            {
+                if (shapes[0].transform.position == targetPosition) targetPosition = transform.position;
+                else if (shapesInCentre())
+                {
+                    meleeAttacking = false;
+                    meleeCD = maxMeleeCD;
+                }
+            }
+        }
+        else
+        {
+            meleeCD -= Time.deltaTime;
+        }
+    }
+
     private bool shapesInRadius()       //Returns true if shapes are in radius
     {
         float distX = transform.position.x - shapes[0].transform.position.x,
@@ -84,5 +124,10 @@ public class Boss_Script : Enemy_Script
             distance = Mathf.Sqrt((distX * distX) + (distY * distY));
 
         return (distance <= radius);
+    }
+
+    private bool shapesInCentre()       //Returns true if shapes are in the centre of boss
+    {
+        return (shapes[0].transform.position == transform.position);
     }
 }
