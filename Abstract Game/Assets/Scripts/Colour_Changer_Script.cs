70df38b88671b8d0b2664d06a99814510bd21caf
Alex Pearson <3peara60@solent.ac.uk>
2018-03-14 17:31:51 +0000

    Trail Testing

--- Abstract Game/Assets/Scripts/Colour_Changer_Script.cs
+++ Abstract Game/Assets/Scripts/Colour_Changer_Script.cs
@@ -9,20 +9,11 @@ public enum colour
 
 public class Colour_Changer_Script : MonoBehaviour
 {
-    //static public Color[] colours;
+    static public float hSliderValueR = 0.0f;
+    static public float hSliderValueG = 0.0f;
+    static public float hSliderValueB = 0.0f;
+    static public float hSliderValueA = 1.0f;
 
-	//void Start ()
- //   {
- //       //colours = new Color[8];
- //       //colours[0] = RGBValuesToColorValues(new Vector4(241, 241, 241, 255));   //White
- //       //colours[1] = RGBValuesToColorValues(new Vector4(51, 51, 51, 255));      //Black
- //       //colours[2] = RGBValuesToColorValues(new Vector4(205, 11, 11, 255));     //Red  
- //       //colours[3] = RGBValuesToColorValues(new Vector4(15, 40, 229, 255));     //Blue     
- //       //colours[4] = RGBValuesToColorValues(new Vector4(196, 204, 46, 255));    //Yellow
- //       //colours[5] = RGBValuesToColorValues(new Vector4(46, 196, 46, 255));     //Green    
- //       //colours[6] = RGBValuesToColorValues(new Vector4(206, 91, 22, 255));     //Orange
- //       //colours[7] = RGBValuesToColorValues(new Vector4(159, 10, 229, 255));    //Purple        
- //   }
 	
     static public void setColour(GameObject thisObject, colour currentColour)
     {
@@ -96,34 +87,34 @@ public class Colour_Changer_Script : MonoBehaviour
 
     static public void setParticleColour(GameObject thisObject, colour currentColour)
     {
-        var main = thisObject.GetComponent<ParticleSystem>().main;      //gets the main part of the particle system
+        var main = thisObject.GetComponent<TrailRenderer>().startColor;      //gets the main part of the particle system
 
         switch (currentColour)
         {
             case colour.white:
-                main.startColor = new Color(241, 241, 241, 1);     //White
+                //main.startColor = new Color(241, 241, 241, 1);     //White
                 break;
             case colour.black:
-                main.startColor = new Color(51, 51, 51, 1);        //Black;
+                //main.startColor = new Color(51, 51, 51, 1);        //Black;
                 break;
             case colour.red:
-                main.startColor = new Color(205, 11, 11, 1);     //Red ;
+                //main.startColor = new Color(205, 11, 11, 1);     //Red ;
                 break;
             case colour.blue:
-                main.startColor = convertColourToGradient(15, 40, 229, 255);     //Blue;
+                main = new Color(15, 40, 229);     //Blue;          15, 40, 229, 255
                 //main.startColor = Color.blue;
                 break;
             case colour.yellow:
-                main.startColor = new Color(196, 204, 46, 1);    //Yellow;
+                //main.startColor = new Color(196, 204, 46, 1);    //Yellow;
                 break;
             case colour.green:
-                main.startColor = new Color(46, 196, 46, 1);     //Green;
+                //main.startColor = new Color(46, 196, 46, 1);     //Green;
                 break;
             case colour.orange:
-                main.startColor = new Color(206, 91, 22, 1);     //Orange;
+                //main.startColor = new Color(206, 91, 22, 1);     //Orange;
                 break;
             case colour.purple:
-                main.startColor = new Color(159, 10, 229, 1);    //Purple;
+                //main.startColor = new Color(159, 10, 229, 1);    //Purple;
                 break;
         }
     }
@@ -138,14 +129,17 @@ public class Colour_Changer_Script : MonoBehaviour
         return values;
     }
 
-    static Color convertColourToGradient(int r, int g, int b, int a)
+    static void onGUI()
     {
-        float rg = r / 255;
-        float gg = g / 255;
-        float bg = b / 255;
-        float ag = a / 255;
+        GUI.Label(new Rect(25, 40, 100, 30), "Red");
+        GUI.Label(new Rect(25, 70, 100, 30), "Green");
+        GUI.Label(new Rect(25, 100, 100, 30), "Blue");
+        GUI.Label(new Rect(25, 130, 100, 30), "Alpha");
 
-        return new Color(rg, gg, bg, ag);
+        hSliderValueR = GUI.HorizontalSlider(new Rect(95, 45, 100, 30), hSliderValueR, 0.0F, 1.0F);
+        hSliderValueG = GUI.HorizontalSlider(new Rect(95, 75, 100, 30), hSliderValueG, 0.0F, 1.0F);
+        hSliderValueB = GUI.HorizontalSlider(new Rect(95, 105, 100, 30), hSliderValueB, 0.0F, 1.0F);
+        hSliderValueA = GUI.HorizontalSlider(new Rect(95, 135, 100, 30), hSliderValueA, 0.0F, 1.0F);
     }
     
 }
